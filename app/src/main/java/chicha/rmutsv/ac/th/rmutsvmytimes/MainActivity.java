bd04be641d4428cd33d719ce81b14c9143aac6c8
Suppachai.m <openpino@hotmail.com>
2018-01-25 11:42:40 +0700

    Add Frangment conplete

--- app/src/main/java/chicha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
+++ app/src/main/java/chicha/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
@@ -3,11 +3,20 @@ package chicha.rmutsv.ac.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import chicha.rmutsv.ac.th.rmutsvmytimes.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment of Activity
+        if (savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment())
+                    .commit();
+        }
+    }//Main Method
+}//Main Class

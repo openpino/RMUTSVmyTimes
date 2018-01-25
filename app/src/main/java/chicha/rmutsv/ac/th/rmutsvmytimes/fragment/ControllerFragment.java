8331fe24ec53bc161c99e623bc2592a55551edb6
Suppachai.m <openpino@hotmail.com>
2018-01-25 16:01:50 +0700

    On off Switch OK

--- app/src/main/java/chicha/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
+++ app/src/main/java/chicha/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
@@ -7,6 +7,13 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import chicha.rmutsv.ac.th.rmutsvmytimes.R;
 
@@ -14,11 +21,73 @@ import chicha.rmutsv.ac.th.rmutsvmytimes.R;
  * Created by Dell on 25/1/2561.
  */
 
-public class ControllerFragment extends Fragment{
+public class ControllerFragment extends Fragment {
+
+
+    //    Explicit
+    private Button zeroButton, onButton, offButton;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+
+        zeroButton = getView().findViewById(R.id.btnZero);
+        onButton = getView().findViewById(R.id.btnOn);
+        offButton = getView().findViewById(R.id.btnOff);
+
+//        Setup Firebase
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+//        Zero Controller
+        zeroButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("myTimes","0");
+                databaseReference.updateChildren(stringObjectMap);
+            }  //onClick
+        });
+
+//        On Controller
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("Switch",1);
+                databaseReference.updateChildren(stringObjectMap);
+              //  Map<Integer,Object> integerObjectMap = new
+            }
+        });
+
+
+
+
+//        Off Controller
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> stringObjectMap = new HashMap<>();
+                stringObjectMap.put("Switch",0);
+                databaseReference.updateChildren(stringObjectMap);
+            }
+        });
+
+
+    } // Main Method
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_controller,container,false);
+        View view = inflater.inflate(R.layout.fragment_controller, container, false);
         return view;
     }
 } //Main Class
